a11edc38ae8e7de8f6ece79c1752c2b0e06b30c6
EnolAyo <enolayo@gmail.com>
2020-09-29 23:40:41 +0200

    [LAB] AVR toolchain tested

--- Labs/01-tools/blink/blink/main.c
+++ Labs/01-tools/blink/blink/main.c
@@ -11,7 +11,10 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define SHORT_DELAY 50      // Delay in miliseconds
+#define SHORT_DELAY 500      // dShort delay in miliseconds
+#define LONG_DELAY 1000			//Long delay in miliseconds
+								//Pause delay in miliseconds
+#define PAUSE 600
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay func
 #endif
@@ -28,6 +31,7 @@
 /**
 * Toggle one LED and use the function from the delay library.
 */
+
 int main(void)
 {
 	// Set pin as output in Data Direction Register
@@ -35,18 +39,58 @@ int main(void)
 	DDRB = DDRB | (1<<LED_GREEN);
 
 	// Set pin LOW in Data Register (LED off)
-	// PORTB = PORTB and 1101 1111
+	// PORTB = PORTB and 1101 1111	
 	PORTB = PORTB & ~(1<<LED_GREEN);
 
 	// Infinite loop
 	while (1)
 	{
 		// Pause several miliseconds
-		_delay_ms(SHORT_DELAY);
+		_delay_ms(PAUSE);
 
 		// Invert LED in Data Register
 		// PORTB = PORTB xor 0010 0000
-		PORTB = PORTB ^ (1<<LED_GREEN);
+		PORTB = PORTB ^ (1<<LED_GREEN);//long blink
+		_delay_ms(LONG_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//short blink
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//short blink
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		//End of letter D
+		PORTB = PORTB ^ (1<<LED_GREEN);//short blink
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		//End of letter E
+		PORTB = PORTB ^ (1<<LED_GREEN);//short blink
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//short blink
+		_delay_ms(SHORT_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//long blink
+		_delay_ms(LONG_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//long blink
+		_delay_ms(LONG_DELAY);
+		PORTB = PORTB ^ (1<<LED_GREEN);//pause
+		_delay_ms(PAUSE);
+		PORTB = PORTB ^ (1<<LED_GREEN);//long blink
+		_delay_ms(LONG_DELAY);
+		//End of 2
+		
+		
+		
+	
 	}
 
 	// Will never reach this
